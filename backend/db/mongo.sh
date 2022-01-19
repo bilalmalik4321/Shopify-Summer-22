f47f9c11fd890b09ab4744b1a68a560612a546f4
Bilal Malik <bilalmalik4321@gmail.com>
2022-01-18 23:18:40 -0500

    updated mongo docker commands to be contained in an sh file

--- backend/db/mongo.sh
+++ backend/db/mongo.sh
@@ -1,2 +1,3 @@
+# set up DB with the following commands
 docker pull mongo
 docker run -d  --name mongo-for-shopify  -p 27888:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
